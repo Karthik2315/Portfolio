2cabd8ab025f0e573b1acdc0034d6ac70c60d82b
Karthik P <padiyil2004@gmail.com>
2026-02-04 15:23:28 +0530

    completed the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,33 @@
+import { navIcons, navLinks } from '#constants'
 import React from 'react'
+import dayjs from 'dayjs'
 
 const Navbar = () => {
+  
   return (
-    <div>
-      
-    </div>
+    <nav>
+      <div>
+        <img src='/images/logo.svg' alt='logo'/>
+        <p className='font-semibold'>Karthik's Portfolio</p>
+        <ul>
+          {navLinks.map((item)=>(
+            <li key={item.id}>
+              <p>{item.name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <ul>
+          {navIcons.map((icon)=>(
+            <li key={icon.id}>
+              <img src={icon.img} className='icon' alt='' />
+            </li>
+          ))}
+        </ul>
+        <time>{dayjs().format('ddd MMM D h:mm A')}</time>
+      </div>
+    </nav>
   )
 }
 
